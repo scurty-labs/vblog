4adfa13afb52521ec442d4422b7741f8d48cb86d
scurty-labs <scurrttie@gmail.com>
2023-01-20 14:28:00 -0800

    aggregate settings.v to settings_entities, settings_routes and settings_service

---
+++ settings_entities.v
@@ -0,0 +1,37 @@
+module main
+
+struct Setting {
+pub mut:
+	id int
+	key string
+	value string
+}
+
+struct Settings {
+pub mut:
+	blog_title string
+	blog_description string
+	accent_foreground string
+	custom_css string
+	posts_per_page string
+}
+
+// -- TODO: Change! Use maps instead. --
+pub fn (mut app App) load_settings() Settings {
+	data := sql app.db { select from Setting }
+	mut settings := Settings{}
+	settings.blog_title = setting_get(data, 'blog_title')
+	settings.blog_description = setting_get(data, 'blog_description')
+	settings.accent_foreground = setting_get(data, 'accent_foreground')
+	settings.custom_css = setting_get(data, 'custom_css')
+	settings.posts_per_page = setting_get(data, 'posts_per_page')
+	return settings
+}
+
+// Cringe...
+fn setting_get(arr []Setting, key string) string {
+	for i in arr {
+		if i.key == key { return i.value }
+	}
+	return ''
+}
---
+++ settings_routes.v
@@ -0,0 +1,19 @@
+module main
+
+import vweb
+
+[post]
+['/save/settings']
+pub fn (mut app App) save_settings() vweb.Result {
+	if !app.auth() { return app.r_home() }
+
+	// Oof. Uwu. What am I working on? CSGO GUI??
+	app.settings.blog_title = app.form['blog_title']
+	app.settings.blog_description = app.form['blog_description']
+	app.settings.accent_foreground = app.form['accent_foreground']
+	app.settings.custom_css = app.form['custom_css']
+	app.settings.posts_per_page = app.form['posts_per_page']
+	app.commit_settings()
+
+	return app.r_home()
+}
